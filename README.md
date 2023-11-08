bf51ae3ce199139b7dbac156350815295c49ee7b
Runzhi He <hrz6976@hotmail.com>
2023-11-08 18:16:11 +0800

    Update README.md

--- README.md
+++ README.md
@@ -4,21 +4,12 @@
 
 ### 安装Python
 
-这个工具需要Python 3.11.2或更高版本。[Asyncio Termination Issue](https://github.com/python/cpython/issues/88050)
-
+因为[Asyncio在Windows上的兼容性问题](https://github.com/python/cpython/issues/88050)，这个工具需要Python 3.11.2或更高版本。
 你可以从[Python官网](https://www.python.org/downloads/)下载Python。
 
 ### Virtualenv
 
 我们建议使用Virtualenv来安装Python依赖项。Virtualenv是一个工具，可以创建一个独立的Python环境，这样你就可以在不影响其他项目的情况下安装Python包。
-
-你可以使用以下命令安装Virtualenv：
-
-```bash
-pip install virtualenv
-```
-
-创建虚拟环境：
 
 ```bash
 py -m venv venv
--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ __version__ = "2023.11"
 __current_dir__ = os.path.dirname(os.path.realpath(__file__))
 
 build_exe_options = {
-    # "include_files": ["stresstester"],
     "packages": ["stresstester"],
     "include_msvcr": True,
     "optimize": 2,
@@ -17,7 +16,13 @@ setup(
     name=__name__,
     version=__version__,
     options={"build_exe": build_exe_options},
-    executables=[Executable("entrypoint.py", target_name="电脑小队系统测试工具.exe", base="Win32GUI", uac_admin=True, icon=os.path.join(__current_dir__, "stresstester", "icon.ico"))],
+    executables=[Executable(
+        "entrypoint.py", 
+        target_name="电脑小队系统测试工具.exe", 
+        base="Win32GUI", 
+        uac_admin=True,
+        icon=os.path.join(__current_dir__, "stresstester", "icon.ico")
+    )],
 )
 
 if os.path.exists(os.path.join(__current_dir__, "dist")):
